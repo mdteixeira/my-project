b74b4a5ee44459e9a1735c0a6fe101728fcad68b
Matheus Teixeira <matheusdiegoteixeira@gmail.com>
2025-08-26 22:56:33 -0300

    deploying to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
-  plugins: [netlifyPlugin(), tailwindcss(), reactRouter({
-serverBuildPath: "build/server/server.js"
-}), tsconfigPaths()],
-  server: {
-    port: 4380
-  }
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
+    server: {
+        port: 4380,
+    },
 });
